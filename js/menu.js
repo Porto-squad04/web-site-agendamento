f117d4262d608578eb2c9f64076ae097ac593a9a
Marcos Guilherme <guitto10@outlook.com>
2024-04-26 21:45:34 -0300

    fase 3

--- js/menu.js
+++ js/menu.js
@@ -13,7 +13,27 @@ menuItem.forEach((item)=>
 
 var btnExp = document.querySelector('#btn-exp')
 var menuLateral = document.querySelector('.menu-lateral')
+var subMenus = document.querySelectorAll('.sub-menu');
 
 btnExp.addEventListener('click', function(){
     menuLateral.classList.toggle('expandir')
-})
+
+    // Se o menu lateral está encolhendo, esconda todos os sub-menus
+    if (!menuLateral.classList.contains('expandir')) {
+        subMenus.forEach(subMenu => {
+            subMenu.style.display = 'none';
+        });
+    }
+});
+
+// SUB-MENU
+document.addEventListener('DOMContentLoaded', (event) => {
+    const subBtns = document.querySelectorAll('.sub-btn');
+    subBtns.forEach(subBtn => {
+        subBtn.addEventListener('click', (event) => {
+            event.preventDefault();
+            const subMenu = subBtn.nextElementSibling;
+            subMenu.style.display = subMenu.style.display === 'none' ? 'block' : 'none';
+        });
+    });
+});
