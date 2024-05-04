474224bc0131e419ae996e9a7fbfb57ee7730b50
Marcos Guilherme <guitto10@outlook.com>
2024-05-03 22:02:08 -0300

    w

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,6 @@
 document.getElementById('open-btn').addEventListener('click', function() {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
   });
-  
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const sideItems = document.querySelectorAll('.side-item');
-  
-    sideItems.forEach((item) => {
-        item.addEventListener('click', (event) => {
-            const submenu = item.querySelector('.submenu');
-            if (submenu.style.display === "none") {
-                submenu.style.display = "block";
-            } else {
-                submenu.style.display = "none";
-            }
-        });
-    });
-  });
   document.getElementById('logout-btn').addEventListener('click', function() {
     window.location.href = 'index.html';
   });
