2ece1757773855cb5040babbbe5130bd2c159be9
Guilherme Silva <sgsouza.173@gmail.com>
2024-05-10 13:50:28 -0300

    Função Saudação e finalização
    Agendamento ajustado!

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,18 @@ document.getElementById('open-btn').addEventListener('click', function() {
     window.location.href = 'index.html';
   });
 
-  
+  /*FUNÇÃO BOM DIA*/
+window.onload = function() {
+  var hora = new Date().getHours();
+  var saudacao;
+
+  if (hora >= 5 && hora < 12) {
+      saudacao = 'Bom dia! 🌤️';
+  } else if (hora >= 12 && hora < 18) {
+      saudacao = 'Boa tarde! 🌇';
+  } else {
+      saudacao = 'Boa noite! 🌙';
+  }
+
+  document.getElementById('saudacao').innerText = saudacao;
+};
